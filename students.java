d1b1f7391c396b74ac85c1f02d5ce163b4a8693f
Mehuli15 <mehulisarkar1511@gmail.com>
2025-03-12 01:22:10 +0530

    Add files via upload

---
+++ students.java
@@ -0,0 +1,7 @@
+class students{
+	public static void main(String [] args) {
+		System.out.println("Student name : Mehuli Sarkar");
+		System.out.println("Roll no : AV.SC.U4CSE24150");
+		System.out.println("Section : B");
+	}
+}
