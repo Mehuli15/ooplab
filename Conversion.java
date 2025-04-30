ce72e9208083c5bf8b3b9a7942be0ec68eb0c9fb
Mehuli15 <mehulisarkar1511@gmail.com>
2025-04-30 11:33:53 +0530

    Add files via upload

---
+++ Conversion.java
@@ -0,0 +1,11 @@
+public class Conversion{
+    public static void main(String[] args) {
+        String line="Welcome! You Are Practicing String Concept";
+        String lower=line.toLowerCase();
+        String upper=line.toUpperCase();
+        System.out.println("Name : Mehuli Sarkar\nClass : CSE-B\nRoll no : AV.SC.U4CSE24150");
+        System.out.println("Lower case : "+lower);
+        System.out.println("Upper case : "+upper);
+        System.out.println("Length of String  : "+line.length());
+    }
+}
