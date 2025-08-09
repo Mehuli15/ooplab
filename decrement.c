74b7964796c3301273e0bd55de684e64be1179e0
Mehuli15 <mehulisarkar1511@gmail.com>
2025-08-09 10:24:26 +0530

    Add files via upload

---
+++ decrement.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main(){
+    int arr[5]={10, 20, 30, 40, 50};
+    int *ptr= arr;
+    ptr++;
+    ptr++;
+    printf("Initial pointer value : \n");
+    printf("Address : %p, Value : %d\n", ptr, *ptr);
+    ptr--;
+    printf("\nAfter decrementing pointer by 1 :\n");
+    printf("Address : %p, Value : %d\n", ptr, *ptr);
+    ptr--;
+    printf("\nAfter decrementing pointer by 1 again :\n");
+    printf("Address : %p, Value : %d\n", ptr, *ptr);
+}
